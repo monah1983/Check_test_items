34c856230916dc63218801a776cb1af71ea7ed9e
ivan <naumau.ivan@gmail.com>
2021-07-02 11:45:16 +0300

    add test in files

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Check_test_items
+Check test_items.py from Stepik course https://stepik.org/lesson/237240/step/9
---
+++ test_items.py
@@ -0,0 +1,5 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_link_button(browser):
+    browser.get(link)
+    browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']").click()
